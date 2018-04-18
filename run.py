8913181b2dbf94b7473d6a71e5ec0236a5701dd7
Kipok <igor.a.gitman@gmail.com>
2018-04-18 14:58:44 -0700

    Add debug parameter

--- run.py
+++ run.py
@@ -40,6 +40,8 @@ def main():
                       help='max_steps for benchmarking')
   parser.add_argument('--bench_start', type=int,
                       help='first step to start counting time for benchmarking')
+  parser.add_argument('--debug', dest='debug', action='store_true',
+                      help='run tensorflow debug mode')
   args, unknown = parser.parse_known_args()
 
   if args.mode not in ['train', 'eval', 'train_eval', 'infer']:
@@ -193,7 +195,7 @@ def main():
       train_model = create_encoder_decoder_loss_model(config=train_config,
                                                       mode="train",
                                                       hvd=hvd)
-      train(train_config, train_model, None, hvd=hvd)
+      train(train_config, train_model, None, hvd=hvd, debug=args.debug)
     elif args.mode == 'train_eval':
       train_model = create_encoder_decoder_loss_model(config=train_config,
                                                       mode="train",
@@ -202,7 +204,7 @@ def main():
                                                      mode="eval",
                                                      hvd=hvd,
                                                      reuse=True)
-      train(train_config, train_model, eval_model, hvd=hvd)
+      train(train_config, train_model, eval_model, hvd=hvd, debug=args.debug)
     elif args.mode == "eval":
       eval_model = create_encoder_decoder_loss_model(
         config=eval_config,
