71868cd500be449194547c9cd033eaafc64d3e7a
Kipok <igor.a.gitman@gmail.com>
2018-04-19 15:42:21 -0700

    Finish config description in using-existing-models

--- open_seq2seq/utils/model_builders.py
+++ open_seq2seq/utils/model_builders.py
@@ -18,14 +18,74 @@ def safe_fill_params(cfg_from, cfgs_to, pm_list):
 
 
 def create_encoder_decoder_loss_model(config, mode, hvd, reuse=False):
-  """
-  Creates Model Graph for Sequence to Sequence model with one Encoder,
-  one Decoder and one Loss
-  :param config: Model configuration
-  :param mode: "train", "train_eval" or "infer"
-  :param hvd: Horovod instance
-  :param reuse: Whether to reuse the variables
-  :return: An instance of Seq2Seq class
+  """Creates a model specified in the configuration file.
+  This function takes in Python config and creates all parts of the model:
+  data layer, encoder, decoder and loss. They are all then combined in one
+  model instance which is returned.
+
+  Args:
+    config (dict): dictionary containing run parameters. For complete list of
+        possible values see "Config parameters" section.
+    mode (str): mode to create the model in. Could be "train", "eval" or "infer".
+    hvd: if Horovod is used, this should be ``horovod.tensorflow`` module.
+        If Horovod is not used it should be None.
+    reuse (bool, optional): whether to reuse variables in the model. Useful
+        for creating evaluation model during training.
+
+  Returns:
+    instance of ``base_model`` class specified in the config.
+
+  Config parameters:
+
+  * **random_seed** (int) --- random seed to use.
+  * **use_horovod** (bool) --- whether to use Horovod for distributed execution.
+  * **num_gpus** (int) --- number of GPUs to use. When ``use_horovod`` is True
+    this parameter is ignored.
+  * **batch_size_per_gpu** (int) --- batch size to use for each GPU.
+  * **num_epochs** (int) --- number of epochs to run training for.
+    This parameter cannot be used if ``max_steps`` is specified.
+  * **max_steps** (int) --- number of steps to run training for.
+    This parameter cannot be used if ``num_epochs`` is specified.
+  * **save_summaries_steps** (int or None) --- how often to save summaries.
+    Setting it to None disables summaries saving.
+  * **print_loss_steps** (int or None) --- how often to print loss during
+    training. Setting it to None disables loss printing.
+  * **print_sample_steps** (int or None) --- how often to print training samples
+    (input sequences, correct answers and model predictions).
+    Setting it to None disables samples printing.
+  * **save_checkpoint_steps** (int or None) --- how often to save model
+    checkpoints. Setting it to None disables checkpoint saving.
+  * **eval_steps** (int) --- how often to run evaluation during training.
+    This parameter is only checked if ``--mode`` argument of ``run.py`` is
+    "train\_eval". If no evaluation is needed you should use "train" mode.
+  * **logdir** (string) --- path to the log directory where all checkpoints and
+    summaries will be saved.
+  * **base_model** (any class derived from
+    :class:`Model <models.model.Model>`) --- base model class to use.
+    Currently can only be :class:`Speech2Text <models.speech2text.Speech2Text>`
+    or
+    :class:`BasicText2TextWithAttention
+    <models.text2text.BasicText2TextWithAttention>`.
+    Note that this parameter is not a string, but an actual Python class, so you
+    will need to add corresponding imports in the configuration file.
+  * **model_params** (dict) --- dictionary with model configuration. For
+    complete list of possible parameters see the corresponding class docs.
+  * **data_layer** (any class derived from
+    :class:`DataLayer <data.data_layer.DataLayer>`) --- data layer class to use.
+  * **data_layer_params** (dict) --- dictionary with data layer configuration.
+    For complete list of possible parameters see the corresponding class docs.
+  * **encoder** (any class derived from
+    :class:`Encoder <encoders.encoder.Encoder>`) --- encoder class to use.
+  * **encoder_params** (dict) --- dictionary with encoder configuration. For
+    complete list of possible parameters see the corresponding class docs.
+  * **decoder** (any class derived from
+    :class:`Decoder <decoders.decoder.Decoder>`) --- decoder class to use.
+  * **decoder_params** (dict) --- dictionary with decoder configuration. For
+    complete list of possible parameters see the corresponding class docs.
+  * **loss** (any class derived from
+    :class:`Loss <losses.loss.Loss>`) --- loss class to use.
+  * **loss_params** (dict) --- dictionary with loss configuration. For
+    complete list of possible parameters see the corresponding class docs.
   """
   check_params(
     config,
