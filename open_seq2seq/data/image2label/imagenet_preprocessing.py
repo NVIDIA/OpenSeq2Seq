2a7527d1e57666b8e83c686ec0d629fd084b8246
Kipok <igor.a.gitman@gmail.com>
2018-05-31 19:54:46 -0700

    Add basic alexnet

--- open_seq2seq/data/image2label/imagenet_preprocessing.py
+++ open_seq2seq/data/image2label/imagenet_preprocessing.py
@@ -13,22 +13,17 @@
 # limitations under the License.
 # ==============================================================================
 """Provides utilities to preprocess images.
-
 Training images are sampled using the provided bounding boxes, and subsequently
 cropped to the sampled bounding box. Images are additionally flipped randomly,
 then resized to the target output size (without aspect-ratio preservation).
-
 Images used during evaluation are resized (with aspect-ratio preservation) and
 centrally cropped.
-
 All images undergo mean color subtraction.
-
 Note that these steps are colloquially referred to as "ResNet preprocessing,"
 and they differ from "VGG preprocessing," which does not use bounding boxes
 and instead does an aspect-preserving resize followed by random crop during
 training. (These both differ from "Inception preprocessing," which introduces
 color distortion steps.)
-
 """
 
 from __future__ import absolute_import
@@ -54,21 +49,17 @@ _NUM_CLASSES = 1001
 
 def _decode_crop_and_flip(image_buffer, bbox, num_channels):
   """Crops the given image to a random part of the image, and randomly flips.
-
   We use the fused decode_and_crop op, which performs better than the two ops
   used separately in series, but note that this requires that the image be
   passed in as an un-decoded string Tensor.
-
   Args:
     image_buffer: scalar string Tensor representing the raw JPEG image buffer.
     bbox: 3-D float Tensor of bounding boxes arranged [1, num_boxes, coords]
       where each coordinate is [0, 1) and the coordinates are arranged as
       [ymin, xmin, ymax, xmax].
     num_channels: Integer depth of the image buffer for decoding.
-
   Returns:
     3-D tensor with cropped image.
-
   """
   # A large fraction of image datasets contain a human-annotated bounding box
   # delineating the region of the image containing the object of interest.  We
@@ -103,12 +94,10 @@ def _decode_crop_and_flip(image_buffer, bbox, num_channels):
 
 def _central_crop(image, crop_height, crop_width):
   """Performs central crops of the given image list.
-
   Args:
     image: a 3-D image tensor
     crop_height: the height of the image following the crop.
     crop_width: the width of the image following the crop.
-
   Returns:
     3-D tensor with cropped image.
   """
@@ -123,23 +112,18 @@ def _central_crop(image, crop_height, crop_width):
       image, [crop_top, crop_left, 0], [crop_height, crop_width, -1])
 
 
-def _mean_image_subtraction(image, means, num_channels):
-  """Subtracts the given means from each image channel.
-
+def _mean_image_subtraction_and_normalization(image, means, num_channels):
+  """Subtracts the given means from each image channel and divides by 127.5.
   For example:
     means = [123.68, 116.779, 103.939]
-    image = _mean_image_subtraction(image, means)
-
+    image = _mean_image_subtraction_and_normalization(image, means)
   Note that the rank of `image` must be known.
-
   Args:
     image: a tensor of size [height, width, C].
     means: a C-vector of values to subtract from each channel.
     num_channels: number of color channels in the image that will be distorted.
-
   Returns:
-    the centered image.
-
+    the centered image and normalized image.
   Raises:
     ValueError: If the rank of `image` is unknown, if `image` has a rank other
       than three or if the number of channels in `image` doesn't match the
@@ -154,21 +138,18 @@ def _mean_image_subtraction(image, means, num_channels):
   # We have a 1-D tensor of means; convert to 3-D.
   means = tf.expand_dims(tf.expand_dims(means, 0), 0)
 
-  return image - means
+  return (image - means) / 127.5
 
 
 def _smallest_size_at_least(height, width, resize_min):
   """Computes new shape with the smallest side equal to `smallest_side`.
-
   Computes new shape with the smallest side equal to `smallest_side` while
   preserving the original aspect ratio.
-
   Args:
     height: an int32 scalar tensor indicating the current height.
     width: an int32 scalar tensor indicating the current width.
     resize_min: A python integer or scalar `Tensor` indicating the size of
       the smallest side after resize.
-
   Returns:
     new_height: an int32 scalar tensor indicating the new height.
     new_width: an int32 scalar tensor indicating the new width.
@@ -190,12 +171,10 @@ def _smallest_size_at_least(height, width, resize_min):
 
 def _aspect_preserving_resize(image, resize_min):
   """Resize images preserving the original aspect ratio.
-
   Args:
     image: A 3-D image `Tensor`.
     resize_min: A python integer or scalar `Tensor` indicating the size of
       the smallest side after resize.
-
   Returns:
     resized_image: A 3-D tensor containing the resized image.
   """
@@ -209,15 +188,12 @@ def _aspect_preserving_resize(image, resize_min):
 
 def _resize_image(image, height, width):
   """Simple wrapper around tf.resize_images.
-
   This is primarily to make sure we use the same `ResizeMethod` and other
   details each time.
-
   Args:
     image: A 3-D image `Tensor`.
     height: The target height for the resized image.
     width: The target width for the resized image.
-
   Returns:
     resized_image: A 3-D tensor containing the resized image. The first two
       dimensions have the shape [height, width].
@@ -230,11 +206,9 @@ def _resize_image(image, height, width):
 def preprocess_image(image_buffer, bbox, output_height, output_width,
                      num_channels, is_training=False):
   """Preprocesses the given image.
-
   Preprocessing includes decoding, cropping, and resizing for both training
   and eval images. Training preprocessing, however, introduces some random
   distortion of the image to improve accuracy.
-
   Args:
     image_buffer: scalar string Tensor representing the raw JPEG image buffer.
     bbox: 3-D float Tensor of bounding boxes arranged [1, num_boxes, coords]
@@ -245,7 +219,6 @@ def preprocess_image(image_buffer, bbox, output_height, output_width,
     num_channels: Integer depth of the image buffer for decoding.
     is_training: `True` if we're preprocessing the image for training and
       `False` otherwise.
-
   Returns:
     A preprocessed image.
   """
@@ -261,16 +234,15 @@ def preprocess_image(image_buffer, bbox, output_height, output_width,
 
   image.set_shape([output_height, output_width, num_channels])
 
-  return _mean_image_subtraction(image, _CHANNEL_MEANS, num_channels)
+  return _mean_image_subtraction_and_normalization(image, _CHANNEL_MEANS,
+                                                   num_channels)
 
 
 def _parse_example_proto(example_serialized):
   """Parses an Example proto containing a training example of an image.
-
   The output of the build_image_data.py image preprocessing script is a dataset
   containing serialized Example protocol buffers. Each Example proto contains
   the following fields (values are included as examples):
-
     image/height: 462
     image/width: 581
     image/colorspace: 'RGB'
@@ -286,11 +258,9 @@ def _parse_example_proto(example_serialized):
     image/format: 'JPEG'
     image/filename: 'ILSVRC2012_val_00041207.JPEG'
     image/encoded: <JPEG encoded string>
-
   Args:
     example_serialized: scalar Tensor tf.string containing a serialized
       Example protocol buffer.
-
   Returns:
     image_buffer: Tensor tf.string containing the contents of a JPEG file.
     label: Tensor tf.int32 containing the label.
@@ -336,15 +306,12 @@ def _parse_example_proto(example_serialized):
 
 def parse_record(raw_record, is_training):
   """Parses a record containing a training example of an image.
-
   The input record is parsed into a label and image, and the image is passed
   through preprocessing steps (cropping, flipping, and so on).
-
   Args:
     raw_record: scalar Tensor tf.string containing a serialized
       Example protocol buffer.
     is_training: A boolean denoting whether the input is for training.
-
   Returns:
     Tuple with processed image tensor and one-hot-encoded label tensor.
   """
