Added griffin-lim; fixed loss masking; added annealed teacher forcing

open_seq2seq/data/text2speech/speech_utils.py
@@ -33,7 +33,7 @@ def get_speech_features_from_file(filename, num_features, pad_to=8,
   :return: (num_time_steps, num_features) NumPy array
   """
   # load audio signal
-  if features_type == "mel":
+  if features_type == "mel" or features_type =="test" or features_type=='spectrogram':
     signal, fs = librosa.core.load(filename, sr=None)
   else:
     fs, signal = wave.read(filename)
@@ -134,14 +134,10 @@ def get_speech_features(signal, fs, num_features, pad_to=8,
 
 
   if features_type == 'spectrogram':
-    frames = psf.sigproc.framesig(sig=signal,
-                                  frame_len=n_window_size,
-                                  frame_step=n_window_stride,
-                                  winfunc=np.hanning)
-
-    # n_window_size = int(n_window_size)
-    # features = np.log1p(psf.sigproc.powspec(frames, NFFT=N_window_size))
-    features = psf.sigproc.logpowspec(frames, NFFT=n_window_size)
+    complex_spec = librosa.stft(y=signal,
+                                n_fft=n_window_size)
+    mag, _ = librosa.magphase(complex_spec)
+    features = np.log(np.clip(mag, a_min=1e-5, a_max=None)).T
     assert num_features <= n_window_size // 2 + 1, \
         "num_features for spectrogram should be <= (fs * window_size // 2 + 1)"
 
@@ -170,7 +166,14 @@ def get_speech_features(signal, fs, num_features, pad_to=8,
                                       power=mag_power)
     features = np.log(np.clip(features, a_min=1e-5, a_max=None)).T
     # features = features.T
-
+  elif features_type == 'test':
+    n_window_size = 512
+    complex_spec = librosa.stft(y=signal,
+                                n_fft=n_window_size)
+    assert num_features <= n_window_size // 2 + 1, \
+        "num_features for spectrogram should be <= (fs * window_size // 2 + 1)"
+    features = np.concatenate((complex_spec.real[:num_features,:],complex_spec.imag[:num_features,:]), axis=0)
+    features = np.log(np.clip(features, a_min=1e-5, a_max=None)).T
   else:
     raise ValueError('Unknown features type: {}'.format(features_type))
 

open_seq2seq/losses/mean_squared_error_loss.py
@@ -13,6 +13,19 @@ class MeanSquaredErrorLoss(Loss):
   def __init__(self, params, model, name="cross_entropy_loss"):
     super(MeanSquaredErrorLoss, self).__init__(params, model, name)
 
+  def get_optional_params(self):
+    """Static method with description of optional parameters.
+
+      Returns:
+        dict:
+            Dictionary containing all the parameters that **can** be
+            included into the ``params`` parameter of the
+            class :meth:`__init__` method.
+    """
+    return {
+      'use_mask': bool,
+    }
+
   def _compute_loss(self, input_dict):
     decoder_predictions = input_dict['decoder_output']['decoder_output']
     post_net_predictions = input_dict['decoder_output']['post_net_output']
@@ -49,8 +62,12 @@ class MeanSquaredErrorLoss(Loss):
     #                  begin=[0, 0, 0],
     #                  size=[-1, current_ts, -1])
 
-    mask = tf.sequence_mask(lengths=tgt_lengths,
+    if self.params.get("use_mask", True):
+      mask = tf.sequence_mask(lengths=tgt_lengths,
                               dtype=tf.float32)
+      mask = tf.expand_dims(mask, axis=-1)
+      decoder_loss = tf.losses.mean_squared_error(labels=labels, predictions=decoder_predictions, weights=mask)
+      post_net_loss = tf.losses.mean_squared_error(labels=labels, predictions=post_net_predictions, weights=mask)
     # mask = tf.expand_dims(mask, axis=-1)
 
     # print(mask.shape)
@@ -58,12 +75,17 @@ class MeanSquaredErrorLoss(Loss):
     # print(predictions.shape)
     # predictions = tf.reshape(predictions, [batch_size, -1, 96])
 
-    decoder_loss = tf.losses.mean_squared_error(labels=labels, predictions=decoder_predictions)
-    post_net_loss = tf.losses.mean_squared_error(labels=labels, predictions=post_net_predictions)
-    loss = tf.reduce_sum((decoder_loss + post_net_loss) * mask)
-    loss /= tf.reduce_sum(mask)
+    else:
+      decoder_loss = tf.losses.mean_squared_error(labels=labels, predictions=decoder_predictions)
+      post_net_loss = tf.losses.mean_squared_error(labels=labels, predictions=post_net_predictions)
+    # print(post_net_predictions.shape)
+    # print(mask.shape)
+    # print(decoder_loss.shape)
+    # loss = tf.reduce_sum((decoder_loss + post_net_loss) * mask)
+    # loss /= tf.reduce_sum(mask)
     # loss = tf.losses.mean_squared_error(labels=labels, predictions=predictions)
     # loss = tf.clip_by_norm(loss, 1.)
+    loss = decoder_loss + post_net_loss
     return loss
 
 class BasicMeanSquaredErrorLoss(Loss):

open_seq2seq/parts/tacotron/tacotron_helper.py
@@ -229,7 +229,7 @@ class TacotronTrainingHelper(Helper):
   """Base abstract class that allows the user to customize sampling."""
 
   def __init__(self, inputs, sequence_length, enable_prenet, 
-              prenet_units=None, prenet_layers=None, sampling_prob=0.,
+              prenet_units=None, prenet_layers=None, sampling_prob=0., anneal_sampling_prob = False,
               time_major=False, sample_ids_shape=None, sample_ids_dtype=None, name=None,
               context=None):
     """Initializer.
@@ -255,7 +255,15 @@ class TacotronTrainingHelper(Helper):
     self._zero_inputs = nest.map_structure(
         lambda inp: array_ops.zeros_like(inp[0, :]), inputs)
     self._batch_size = array_ops.size(sequence_length)
-    self.sampling_prob = sampling_prob
+    self.seed = 0
+    self.anneal_sampling_prob = anneal_sampling_prob
+    if anneal_sampling_prob:
+      ## Currently hard-coded
+      curr_epoch = tf.div(tf.cast(tf.train.get_or_create_global_step(),tf.float32), tf.constant(10480./48.))
+      curr_step = tf.floor(tf.div(curr_epoch,tf.constant(500/20)))
+      self.sampling_prob = tf.div(curr_step,tf.constant(20.))
+    else:
+      self.sampling_prob = sampling_prob
     # self.context = context
 
     ## Create finished projection layer
@@ -324,7 +332,7 @@ class TacotronTrainingHelper(Helper):
         out = layer(out)
 
 
-      if self.sampling_prob > 0:
+      if self.anneal_sampling_prob or self.sampling_prob > 0:
         select_sampler = bernoulli.Bernoulli(
             probs=self.sampling_prob, dtype=dtypes.bool)
         select_sample = select_sampler.sample(