63d075adf6c60c96f553a14da36359000a83daa0
Vahid Noroozi <vnoroozi@nvidia.com>
2018-06-20 15:02:58 -0700

    Test after cleaning.

--- example_configs/text2text/en-de-convs2s.py
+++ example_configs/text2text/en-de-convs2s.py
@@ -122,7 +122,7 @@ train_params = {
     "source_file": data_root+"train.tok.clean.bpe.32000.en",
     "target_file": data_root+"train.tok.clean.bpe.32000.de",
     "delimiter": " ",
-    "shuffle": False,
+    "shuffle": True,
     "repeat": True,
     "map_parallel_calls": 16,
     "prefetch_buffer_size": 8,
