2f00013c913eccc4353f4132f94a981f7862220f
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2018-06-22 17:39:05 -0700

    adjust config

--- example_configs/text2text/en-de-gnmt-like-4GPUs.py
+++ example_configs/text2text/en-de-gnmt-like-4GPUs.py
@@ -10,8 +10,7 @@ from open_seq2seq.losses import BasicSequenceLoss
 from open_seq2seq.data.text2text.text2text import SpecialTextTokens
 from open_seq2seq.optimizers.lr_policies import exp_decay
 
-#data_root = "[REPLACE THIS TO THE PATH WITH YOUR WMT DATA]"
-data_root = "/mnt/D1/Data/Translate/wmt16/"
+data_root = "[REPLACE THIS TO THE PATH WITH YOUR WMT DATA]"
 
 base_model = Text2Text
 
