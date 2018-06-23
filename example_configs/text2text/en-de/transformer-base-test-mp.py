bc676571f37ddc6465e7d5317ed6bce1957fb89b
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2018-06-22 23:37:37 -0700

    change transformer config

--- example_configs/text2text/en-de/transformer-base-test-mp.py
+++ example_configs/text2text/en-de/transformer-base-test-mp.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 from open_seq2seq.models import Text2Text
 from open_seq2seq.encoders import TransformerEncoder
 from open_seq2seq.decoders import TransformerDecoder
-from open_seq2seq.data.text2text.text2text import TransformerDataLayer
+from open_seq2seq.data.text2text.text2text import ParallelTextDataLayer
 from open_seq2seq.losses import PaddedCrossEntropyLossWithSmoothing
 from open_seq2seq.data.text2text.text2text import SpecialTextTokens
 from open_seq2seq.data.text2text.tokenizer import EOS_ID
@@ -16,44 +16,37 @@ https://arxiv.org/abs/1706.03762
 
 base_model = Text2Text
 d_model = 512
-num_layers = 6
 
-data_root = "/tmp/translate_ende/"
+data_root = "/data/wmt16_s2s/"
 
 base_params = {
   "use_horovod": False,
   "num_gpus": 1,
-  "batch_size_per_gpu": 4096,  # this size is in tokens
-  "max_steps": 500000,
+  "batch_size_per_gpu": 128,  # this size is in sentence pairs
+  "max_steps": 70,#500000,
   "save_summaries_steps": 50,
   "print_loss_steps": 50,
   "print_samples_steps": 50,
   "eval_steps": 4001,
-  "save_checkpoint_steps": 1000,
+  "save_checkpoint_steps": 4000,
   "logdir": "Transformer-MP",
-  #"dtype": tf.float32,
   "dtype": "mixed",
-  "loss_scaling": "Backoff",
+  "automatic_loss_scaling": "Backoff",
   "optimizer": tf.contrib.opt.LazyAdamOptimizer,
   "optimizer_params": {
     "beta1": 0.9,
     "beta2": 0.997,
     "epsilon": 1e-09,
   },
-
   "lr_policy": transformer_policy,
   "lr_policy_params": {
     "learning_rate": 2.0,
     "warmup_steps": 16000,
     "d_model": d_model,
   },
-
-  # "summaries": ['learning_rate', 'variables', 'gradients', 'larc_summaries',
-  #              'variable_norm', 'gradient_norm', 'global_gradient_norm'],
-
   "encoder": TransformerEncoder,
   "encoder_params": {
-    "encoder_layers": num_layers,
+    "encoder_layers": 6,
     "hidden_size": d_model,
     "num_heads": 8,
     "attention_dropout": 0.1,
@@ -66,7 +59,7 @@ base_params = {
   "decoder": TransformerDecoder,
   "decoder_params": {
     "layer_postprocess_dropout": 0.1,
-    "num_hidden_layers": num_layers,
+    "num_hidden_layers": 6,
     "hidden_size": d_model,
     "num_heads": 8,
     "attention_dropout": 0.1,
@@ -88,46 +81,53 @@ base_params = {
 }
 
 train_params = {
-  "data_layer": TransformerDataLayer,
+  "data_layer": ParallelTextDataLayer,
   "data_layer_params": {
-    'data_dir': data_root,
-    'file_pattern': "*train*",
-    'src_vocab_file': data_root + "vocab.ende.32768",
-    'max_length': 256,
-    'shuffle': True,
-    'repeat': 100000,
-    'mode': 'train',
-    "delimiter": ' ',
+    "pad_vocab_to_eight": True,
+    "src_vocab_file": data_root + "vocab.bpe.32000",
+    "tgt_vocab_file": data_root + "vocab.bpe.32000",
+    "source_file": data_root + "train.tok.clean.bpe.32000.en",
+    "target_file": data_root + "train.tok.clean.bpe.32000.de",
+    "delimiter": " ",
+    "shuffle": True,
+    "repeat": True,
+    "map_parallel_calls": 16,
+    "prefetch_buffer_size": 2,
+    "max_length": 56,
   },
 }
 
 eval_params = {
-  "batch_size_per_gpu": 256,
-  "data_layer": TransformerDataLayer,
+  "batch_size_per_gpu": 16,
+  "data_layer": ParallelTextDataLayer,
   "data_layer_params": {
-    'data_dir': data_root,
-    'file_pattern': "*dev*",
-    'src_vocab_file': data_root + "vocab.ende.32768",
-    'max_length': 256,
-    'shuffle': False,
-    'repeat': 1,
-    'mode': 'train',
-    "delimiter": ' ',
+    "pad_vocab_to_eight": True,
+    "src_vocab_file": data_root + "vocab.bpe.32000",
+    "tgt_vocab_file": data_root + "vocab.bpe.32000",
+    "source_file": data_root + "newstest2013.tok.bpe.32000.en",
+    "target_file": data_root + "newstest2013.tok.bpe.32000.de",
+    "delimiter": " ",
+    "shuffle": False,
+    "repeat": True,
+    "map_parallel_calls": 16,
+    "prefetch_buffer_size": 1,
+    "max_length": 32,
   },
 }
 
 infer_params = {
-  "batch_size_per_gpu": 64, # it is now in samples, not tokens
-  "data_layer": TransformerDataLayer,
+  "batch_size_per_gpu": 1,
+  "data_layer": ParallelTextDataLayer,
   "data_layer_params": {
-    'data_dir': data_root,
-    'file_pattern': "*test*",
-    'batch_in_tokens': False, # this is necessary to preserve the order
-    'src_vocab_file': data_root + "vocab.ende.32768",
-    'max_length': 256,
-    'shuffle': False,
-    'repeat': 1,
-    'mode': 'train',
-    "delimiter": ' ',
+    "pad_vocab_to_eight": True,
+    "src_vocab_file": data_root + "vocab.bpe.32000",
+    "tgt_vocab_file": data_root + "vocab.bpe.32000",
+    "source_file": data_root + "newstest2014.tok.bpe.32000.en",
+    # this is intentional
+    "target_file": data_root + "newstest2014.tok.bpe.32000.en",
+    "delimiter": " ",
+    "shuffle": False,
+    "repeat": False,
+    "max_length": 512,
   },
 }
