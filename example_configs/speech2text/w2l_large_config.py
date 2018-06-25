03e56e4aab05de8fe5f625d0d8dcb00e3039da37
Ravi <xravitejax@gmail.com>
2018-06-25 11:44:53 -0700

    Hyperparameter Tuning: Push to Git

--- example_configs/speech2text/w2l_large_config.py
+++ example_configs/speech2text/w2l_large_config.py
@@ -84,7 +84,7 @@ base_params = {
       },
     ],
 
-    "dropout_keep_prob": 0.8,
+    "dropout_keep_prob": 1.0,
 
     "initializer": tf.contrib.layers.xavier_initializer,
     "initializer_params": {
