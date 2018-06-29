025699654bb032b22f4896298148ff3d6ec3a721
Vahid Noroozi <vnoroozi@nvidia.com>
2018-06-29 16:25:06 -0700

    disabled extra position embedding.

--- example_configs/text2text/en-de/en-de-convs2s.py
+++ example_configs/text2text/en-de/en-de-convs2s.py
@@ -159,7 +159,7 @@ eval_params = {
 }
 
 infer_params = {
-  "batch_size_per_gpu": 1,
+  "batch_size_per_gpu": 32,
   "data_layer": ParallelTextDataLayer,
   "data_layer_params": {
     "pad_vocab_to_eight": False,
