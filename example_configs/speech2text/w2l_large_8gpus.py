d800a7b2a867c5996175ba378014e2d4b6658382
Ravi Teja Gadde <xravitejax@gmail.com>
2018-07-08 03:39:03 -0700

    Update w2l_large_8gpus.py

--- example_configs/speech2text/w2l_large_8gpus.py
+++ example_configs/speech2text/w2l_large_8gpus.py
@@ -53,7 +53,7 @@ base_params = {
   "encoder_params": {
     "convnet_layers": [
       {
-        "type": "conv1d", "repeat" : 3,
+        "type": "conv1d", "repeat" : 5,
         "kernel_size": [7], "stride": [1],
         "num_channels": 200, "padding": "SAME"
       },
