8e84d51044d82fbe2a1fd1e5cb965d2ed57948b2
Ravi Teja Gadde <xravitejax@gmail.com>
2018-07-15 21:26:19 -0700

    Update w2l_large_8gpus_mp.py

--- example_configs/speech2text/w2l_large_8gpus_mp.py
+++ example_configs/speech2text/w2l_large_8gpus_mp.py
@@ -4,7 +4,7 @@ from open_seq2seq.encoders import Wave2LetterEncoder
 from open_seq2seq.decoders import FullyConnectedCTCDecoder
 from open_seq2seq.data import Speech2TextDataLayer
 from open_seq2seq.losses import CTCLoss
-from open_seq2seq.optimizers.lr_policies import poly_decay
+from open_seq2seq.optimizers.lr_policies import *
 
 
 base_model = Speech2Text
@@ -12,10 +12,10 @@ base_model = Speech2Text
 base_params = {
   "random_seed": 0,
   "use_horovod": True,
-  "num_epochs": 20,
+  "num_epochs": 40,
 
   "num_gpus": 8,
-  "batch_size_per_gpu": 64,
+  "batch_size_per_gpu": 32,
   "iter_size": 1,
 
   "save_summaries_steps": 100,
@@ -30,12 +30,13 @@ base_params = {
     "momentum": 0.90,
   },
   "lr_policy": poly_decay,
+  #"lr_policy": fixed_lr,
   "lr_policy_params": {
-    "learning_rate": 0.001,
+    "learning_rate": 0.1,
     "power": 2.0,
   },
   "larc_params": {
-    "larc_eta": 0.001,
+    "larc_eta": 0.004,
   },
 
   "regularizer": tf.contrib.layers.l2_regularizer,
@@ -56,37 +57,44 @@ base_params = {
       {
         "type": "conv1d", "repeat" : 3,
         "kernel_size": [11], "stride": [1],
-        "num_channels": 256, "padding": "SAME"
+        "num_channels": 256, "padding": "SAME",
+        "dropout_keep_prob": 0.8,
       },
       {
         "type": "conv1d", "repeat" : 3,
         "kernel_size": [13], "stride": [1],
-        "num_channels": 384, "padding": "SAME"
+        "num_channels": 384, "padding": "SAME",
+        "dropout_keep_prob": 0.8,
       },
       {
         "type": "conv1d", "repeat" : 3,
         "kernel_size": [17], "stride": [1],
-        "num_channels": 512, "padding": "SAME"
+        "num_channels": 512, "padding": "SAME",
+        "dropout_keep_prob": 0.8,
       },
       {
         "type": "conv1d", "repeat" : 3,
         "kernel_size": [21], "stride": [1],
-        "num_channels": 640, "padding": "SAME"
+        "num_channels": 640, "padding": "SAME",
+        "dropout_keep_prob": 0.7,
       },
       {
         "type": "conv1d", "repeat" : 3,
         "kernel_size": [25], "stride": [1],
-        "num_channels": 768, "padding": "SAME"
+        "num_channels": 768, "padding": "SAME",
+        "dropout_keep_prob": 0.7,
       },
       {
         "type": "conv1d", "repeat" : 1,
         "kernel_size": [29], "stride": [1],
-        "num_channels": 896, "padding": "SAME"
+        "num_channels": 896, "padding": "SAME",
+        "dropout_keep_prob": 0.6,
       },
       {
         "type": "conv1d", "repeat" : 1,
         "kernel_size": [1], "stride": [1],
-        "num_channels": 1024, "padding": "SAME"
+        "num_channels": 1024, "padding": "SAME",
+        "dropout_keep_prob": 0.6,
       },
     ],
 
