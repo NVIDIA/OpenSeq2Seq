bf2b952bfa893826255fbd0b872ad165839a8f73
Jason <jasoli@nvidia.com>
2018-07-16 16:31:37 -0700

    helper

--- open_seq2seq/parts/tacotron/tacotron_helper.py
+++ open_seq2seq/parts/tacotron/tacotron_helper.py
@@ -45,7 +45,7 @@ def _unstack_ta(inp):
 
 
 class TacotronTrainingHelper(Helper):
-  """Helper funciton for training. Can be used for teacher forcing or scheduled 
+  """Helper funciton for training. Can be used for teacher forcing or scheduled
   sampling"""
 
   def __init__(
@@ -59,7 +59,6 @@ class TacotronTrainingHelper(Helper):
       time_major=False,
       sample_ids_shape=None,
       sample_ids_dtype=None,
-      name=None,
       mask_decoder_sequence=None
   ):
     """Initializer.
@@ -73,7 +72,6 @@ class TacotronTrainingHelper(Helper):
       anneal_teacher_forcing (float): see tacotron 2 decoder
       stop_gradient (float): see tacotron 2 decoder
       time_major (bool): (float): see tacotron 2 decoder
-      name (str): name for layer
       mask_decoder_sequence (bool): whether to pass finished when the decoder
         passed the sequence_length input or to pass unfinished to dynamic_decode
     """
@@ -85,19 +83,19 @@ class TacotronTrainingHelper(Helper):
     self._input_tas = nest.map_structure(_unstack_ta, inputs)
     self._sequence_length = sequence_length
     self._batch_size = array_ops.size(sequence_length)
-    self.seed = None
-    self.sampling_prob = sampling_prob
-    self.anneal_teacher_forcing = anneal_teacher_forcing
-    self.stop_gradient = stop_gradient
-    self.mask_decoder_sequence = mask_decoder_sequence
-    self.prenet = prenet
+    self._seed = None
+    self._sampling_prob = sampling_prob
+    self._anneal_teacher_forcing = anneal_teacher_forcing
+    self._stop_gradient = stop_gradient
+    self._mask_decoder_sequence = mask_decoder_sequence
+    self._prenet = prenet
     self._zero_inputs = nest.map_structure(
         lambda inp: array_ops.zeros_like(inp[0, :]), inputs
     )
     self._start_inputs = self._zero_inputs
     if prenet is not None:
-      self._start_inputs = self.prenet(self._zero_inputs)
-    self.last_dim = self._start_inputs.get_shape()[-1]
+      self._start_inputs = self._prenet(self._zero_inputs)
+    self._last_dim = self._start_inputs.get_shape()[-1]
 
   @property
   def batch_size(self):
@@ -125,7 +123,7 @@ class TacotronTrainingHelper(Helper):
     # Applies the fully connected pre-net to the decoder
     # Also decides whether the decoder is finished
     next_time = time + 1
-    if self.mask_decoder_sequence:
+    if self._mask_decoder_sequence:
       finished = (next_time >= self._sequence_length)
     else:
       finished = array_ops.tile([False], [self._batch_size])
@@ -133,23 +131,23 @@ class TacotronTrainingHelper(Helper):
 
     def get_next_input(inp, out):
       next_input = inp.read(time)
-      if self.stop_gradient:
+      if self._stop_gradient:
         next_input = tf.stop_gradient(next_input)
         out = tf.stop_gradient(out)
-      if self.prenet is not None:
-        next_input = self.prenet(next_input)
-        out = self.prenet(out)
-      if self.anneal_teacher_forcing or self.sampling_prob > 0:
+      if self._prenet is not None:
+        next_input = self._prenet(next_input)
+        out = self._prenet(out)
+      if self._anneal_teacher_forcing or self._sampling_prob > 0:
         select_sampler = bernoulli.Bernoulli(
-            probs=self.sampling_prob, dtype=dtypes.bool
+            probs=self._sampling_prob, dtype=dtypes.bool
         )
         select_sample = select_sampler.sample(
-            sample_shape=(self.batch_size, 1), seed=self.seed
+            sample_shape=(self.batch_size, 1), seed=self._seed
         )
-        select_sample = tf.tile(select_sample, [1, self.last_dim])
+        select_sample = tf.tile(select_sample, [1, self._last_dim])
         sample_ids = array_ops.where(
             select_sample, out,
-            gen_array_ops.fill([self.batch_size, self.last_dim], -20.)
+            gen_array_ops.fill([self.batch_size, self._last_dim], -20.)
         )
         where_sampling = math_ops.cast(
             array_ops.where(sample_ids > -20), dtypes.int32
@@ -195,7 +193,6 @@ class TacotronHelper(Helper):
       time_major=False,
       sample_ids_shape=None,
       sample_ids_dtype=None,
-      name=None,
       mask_decoder_sequence=None
   ):
     """Initializer.
@@ -208,21 +205,23 @@ class TacotronHelper(Helper):
       anneal_teacher_forcing (float): see tacotron 2 decoder
       stop_gradient (float): see tacotron 2 decoder
       time_major (bool): (float): see tacotron 2 decoder
-      name (str): name for layer
       mask_decoder_sequence (bool): whether to pass finished when the decoder
         passed the sequence_length input or to pass unfinished to dynamic_decode
     """
     self._sample_ids_shape = tensor_shape.TensorShape(sample_ids_shape or [])
     self._sample_ids_dtype = sample_ids_dtype or dtypes.int32
-
     self._batch_size = inputs.get_shape()[0]
-    self.mask_decoder_sequence = mask_decoder_sequence
+    self._mask_decoder_sequence = mask_decoder_sequence
 
-    self.prenet = prenet
+    if not time_major:
+      inputs = nest.map_structure(_transpose_batch_time, inputs)
+
+
+    self._prenet = prenet
     if prenet is None:
       self._start_inputs = inputs
     else:
-      self._start_inputs = self.prenet(inputs)
+      self._start_inputs = self._prenet(inputs)
 
   @property
   def batch_size(self):
@@ -258,7 +257,7 @@ class TacotronHelper(Helper):
     # Applies the fully connected pre-net to the decoder
     # Also decides whether the decoder is finished
     next_time = time + 1
-    if self.mask_decoder_sequence:
+    if self._mask_decoder_sequence:
       stop_token_predictions = tf.sigmoid(stop_token_predictions)
       finished = tf.cast(tf.round(stop_token_predictions), tf.bool)
       finished = tf.squeeze(finished)
@@ -267,8 +266,8 @@ class TacotronHelper(Helper):
     all_finished = math_ops.reduce_all(finished)
 
     def get_next_input(out):
-      if self.prenet is not None:
-        out = self.prenet(out)
+      if self._prenet is not None:
+        out = self._prenet(out)
       return out
 
     next_inputs = control_flow_ops.cond(
