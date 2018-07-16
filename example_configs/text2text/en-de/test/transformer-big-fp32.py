e1a39d92a105bc736842f92425351569a07336cf
Oleksii Kuchaiev <oleksii@ngvpn01-167-174.dyn.scz.us.nvidia.com>
2018-07-16 19:27:25 +1000

    try some test configs

---
+++ example_configs/text2text/en-de/test/transformer-big-fp32.py
@@ -0,0 +1,139 @@
+from __future__ import absolute_import, division, print_function
+from open_seq2seq.models import Text2Text
+from open_seq2seq.encoders import TransformerEncoder
+from open_seq2seq.decoders import TransformerDecoder
+from open_seq2seq.data.text2text.text2text import ParallelTextDataLayer
+from open_seq2seq.losses import PaddedCrossEntropyLossWithSmoothing
+from open_seq2seq.data.text2text.text2text import SpecialTextTokens
+from open_seq2seq.data.text2text.tokenizer import EOS_ID
+from open_seq2seq.optimizers.lr_policies import transformer_policy
+import tensorflow as tf
+
+"""
+This configuration file describes a variant of Transformer model from
+https://arxiv.org/abs/1706.03762
+"""
+
+base_model = Text2Text
+d_model = 512
+num_layers = 6
+
+# REPLACE THIS TO THE PATH WITH YOUR WMT DATA
+data_root = "/data/wmt16_s2s/"
+
+base_params = {
+  "use_horovod": True,
+  "num_gpus": 1,
+  "batch_size_per_gpu": 128,  # this size is in sentence pairs
+  "max_steps": 50000,
+  "save_summaries_steps": 50,
+  "print_loss_steps": 50,
+  "print_samples_steps": 50,
+  "eval_steps": 4001,
+  "save_checkpoint_steps": 4000,
+  "logdir": "Transformer-FP32",
+  "dtype": tf.float32,
+  #"dtype": "mixed",
+  #"loss_scaling": "Backoff",
+
+  "optimizer": tf.contrib.opt.LazyAdamOptimizer,
+  "optimizer_params": {
+    "beta1": 0.9,
+    "beta2": 0.997,
+    "epsilon": 1e-09,
+  },
+
+  "lr_policy": transformer_policy,
+  "lr_policy_params": {
+    "learning_rate": 2.0,
+    "warmup_steps": 2000,
+    "d_model": d_model,
+  },
+
+  "encoder": TransformerEncoder,
+  "encoder_params": {
+    "encoder_layers": num_layers,
+    "hidden_size": d_model,
+    "num_heads": 8,
+    "attention_dropout": 0.1,
+    "filter_size": 4 * d_model,
+    "relu_dropout": 0.1,
+    "layer_postprocess_dropout": 0.1,
+    "pad_embeddings_2_eight": True,
+  },
+
+  "decoder": TransformerDecoder,
+  "decoder_params": {
+    "layer_postprocess_dropout": 0.1,
+    "num_hidden_layers": num_layers,
+    "hidden_size": d_model,
+    "num_heads": 8,
+    "attention_dropout": 0.1,
+    "relu_dropout": 0.1,
+    "filter_size": 4 * d_model,
+    "beam_size": 4,
+    "alpha": 0.6,
+    "extra_decode_length": 50,
+    "EOS_ID": EOS_ID,
+    "GO_SYMBOL": SpecialTextTokens.S_ID.value,
+    "END_SYMBOL": SpecialTextTokens.EOS_ID.value,
+    "PAD_SYMBOL": SpecialTextTokens.PAD_ID.value,
+  },
+
+  "loss": PaddedCrossEntropyLossWithSmoothing,
+  "loss_params": {
+    "label_smoothing": 0.1,
+  }
+}
+
+train_params = {
+  "data_layer": ParallelTextDataLayer,
+  "data_layer_params": {
+    "pad_vocab_to_eight": True,
+    "src_vocab_file": data_root + "vocab.bpe.32000",
+    "tgt_vocab_file": data_root + "vocab.bpe.32000",
+    "source_file": data_root + "train.tok.clean.bpe.32000.en",
+    "target_file": data_root + "train.tok.clean.bpe.32000.de",
+    "delimiter": " ",
+    "shuffle": True,
+    "repeat": True,
+    "map_parallel_calls": 16,
+    "prefetch_buffer_size": 2,
+    "max_length": 56,
+  },
+}
+
+eval_params = {
+  "batch_size_per_gpu": 16,
+  "data_layer": ParallelTextDataLayer,
+  "data_layer_params": {
+    "pad_vocab_to_eight": True,
+    "src_vocab_file": data_root + "vocab.bpe.32000",
+    "tgt_vocab_file": data_root + "vocab.bpe.32000",
+    "source_file": data_root + "newstest2013.tok.bpe.32000.en",
+    "target_file": data_root + "newstest2013.tok.bpe.32000.de",
+    "delimiter": " ",
+    "shuffle": False,
+    "repeat": True,
+    "map_parallel_calls": 16,
+    "prefetch_buffer_size": 1,
+    "max_length": 32,
+  },
+}
+
+infer_params = {
+  "batch_size_per_gpu": 1,
+  "data_layer": ParallelTextDataLayer,
+  "data_layer_params": {
+    "pad_vocab_to_eight": True,
+    "src_vocab_file": data_root + "vocab.bpe.32000",
+    "tgt_vocab_file": data_root + "vocab.bpe.32000",
+    "source_file": data_root + "newstest2014.tok.bpe.32000.en",
+    # this is intentional
+    "target_file": data_root + "newstest2014.tok.bpe.32000.en",
+    "delimiter": " ",
+    "shuffle": False,
+    "repeat": False,
+    "max_length": 512,
+  },
+}
