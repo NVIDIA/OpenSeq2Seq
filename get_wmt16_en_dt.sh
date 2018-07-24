2ac94357935b3bee8ec00c52e91273322ffba642
Vahid Noroozi <vnoroozi@nvidia.com>
2018-07-24 12:49:47 -0700

    Added shuffle_buffer_size to the text layer. Added shuffing to the get_wmt16_en_dt.sh.

--- get_wmt16_en_dt.sh
+++ get_wmt16_en_dt.sh
@@ -15,5 +15,31 @@ cd "$TEMP_DIR"
 git clone https://github.com/google/seq2seq
 cd seq2seq
 ./bin/data/wmt16_en_de.sh
+
+
+declare -a train_list_en=("train.tok.clean.bpe.32000.en"
+                           "train.tok.bpe.32000.en"
+                           "train.tok.clean.en"
+                           "train.tok.en"
+                           "train.clean.en"
+                           "train.en")
+
+declare -a train_list_de=("train.tok.clean.bpe.32000.de"
+                           "train.tok.bpe.32000.de"
+                           "train.tok.clean.de"
+                           "train.tok.de"
+                           "train.clean.de"
+                           "train.de")
+
+list_size=${#train_list_en[@]}
+
+for (( i=0; i<${list_size}; i++ ));
+do
+  shuf --random-source=${OUTPUT_DIR}/${train_list_en[$i]} ${OUTPUT_DIR}/${train_list_en[$i]} > ${OUTPUT_DIR}/${train_list_en[$i]}.shuffled
+  shuf --random-source=${OUTPUT_DIR}/${train_list_en[$i]} ${OUTPUT_DIR}/${train_list_de[$i]} > ${OUTPUT_DIR}/${train_list_de[$i]}.shuffled
+  mv ${OUTPUT_DIR}/${train_list_en[$i]}.shuffled ${OUTPUT_DIR}/${train_list_en[$i]}
+  mv ${OUTPUT_DIR}/${train_list_de[$i]}.shuffled ${OUTPUT_DIR}/${train_list_de[$i]}
+done
+
 cd "$SCRIPT_DIR"
 rm -rf "$TEMP_DIR"
