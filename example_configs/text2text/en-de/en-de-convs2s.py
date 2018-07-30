6862de42371de646d0832f49a0800282669afa2d
Oleksii Kuchaiev <okuchaiev@users.noreply.github.com>
2018-07-30 16:38:58 -0700

    Update en-de-convs2s.py

--- example_configs/text2text/en-de/en-de-convs2s.py
+++ example_configs/text2text/en-de/en-de-convs2s.py
@@ -36,8 +36,8 @@ max_steps = 310000
 max_length = 64
 
 base_params = {
-  "use_horovod": False,
-  "num_gpus": 8,
+  "use_horovod": True,
+  "num_gpus": 1, # Use 8 horovod workers to train on 8 GPUs
 
   # max_step is set for 35 epochs on 8 gpus with batch size of 64,
   # 4.5M is the size of the dataset
