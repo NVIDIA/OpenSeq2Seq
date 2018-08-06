7be4ffc78667cc481413258291c14b6792a54880
Vahid Noroozi <vnoroozi@nvidia.com>
2018-08-06 12:07:46 -0700

    Test mixed batch norm with relu.

--- example_configs/text2text/en-de/en-de-convs2s_plus.py
+++ example_configs/text2text/en-de/en-de-convs2s_plus.py
@@ -33,13 +33,13 @@ num_gpus = 8
 
 iter_size = 1
 dtype = "mixed"  # "mixed" or tf.float32
-shuffle_train = True
-use_horovod = True
+shuffle_train = False
+use_horovod = False
 
 max_steps = int((4500000 / (num_gpus * batch_size * iter_size)) * epoch_num)
 
 conv_act = None #tf.nn.relu tf.nn.tanh gated_linear_units
-normalization_type = "weight_norm"  #weight_norm or "batch_norm" or None
+normalization_type = "batch_norm"  #weight_norm or "batch_norm" or None
 scaling_factor = math.sqrt(0.5) #changed here
 
 base_params = {
