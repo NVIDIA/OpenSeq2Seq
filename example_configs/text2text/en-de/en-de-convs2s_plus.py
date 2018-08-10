025bc483705048d43d0ede648dbffe9fa21cce03
Vahid Noroozi <vnoroozi@nvidia.com>
2018-08-09 20:20:32 -0700

    Test batch norm with reg 1e-4

--- example_configs/text2text/en-de/en-de-convs2s_plus.py
+++ example_configs/text2text/en-de/en-de-convs2s_plus.py
@@ -33,7 +33,7 @@ num_gpus = 8
 
 iter_size = 1
 dtype = "mixed" #tf.float32 #tf.float32 #  #
-shuffle_train = True
+shuffle_train = False
 use_horovod = True
 
 max_steps = int((4500000 / (num_gpus * batch_size * iter_size)) * epoch_num)
