b3982e84dfc04ab6dcbfc0618299f8db641f35f3
Vahid Noroozi <vnoroozi@nvidia.com>
2018-08-13 18:39:29 -0700

    Test batch norm.

--- example_configs/text2text/en-de/en-de-convs2s_plus.py
+++ example_configs/text2text/en-de/en-de-convs2s_plus.py
@@ -39,7 +39,7 @@ use_horovod = True
 max_steps = int((4500000 / (num_gpus * batch_size * iter_size)) * epoch_num)
 
 conv_act = None #tf.nn.relu tf.nn.tanh gated_linear_units
-normalization_type = "batch_norm"  #weight_norm or "batch_norm" or None
+normalization_type = "layer_norm"  #weight_norm or "batch_norm" or None
 scaling_factor = 1.0 #math.sqrt(0.5) #changed here
 
 inti_var = 1e-3
