135a1a263157ae3eeaa194ca9f99a1afd06ed275
Ravi <xravitejax@gmail.com>
2018-08-13 18:57:56 -0700

    Hyperparameter Tuning

--- example_configs/speech2text/jca_large_8gpus_wl.py
+++ example_configs/speech2text/jca_large_8gpus_wl.py
@@ -90,7 +90,7 @@ base_params = {
 
         "dropout_keep_prob": 0.8,
 
-        "residual_connections": True,
+        "residual_connections": False,
 
         "initializer": tf.contrib.layers.xavier_initializer,
         "initializer_params": {
