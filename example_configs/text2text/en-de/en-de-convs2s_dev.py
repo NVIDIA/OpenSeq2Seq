10ed880d76138a724cd79fcf1b59d27e9734d4a7
Vahid Noroozi <vnoroozi@nvidia.com>
2018-08-14 17:14:54 -0700

    Test a beast. emb=1024, 20 layers of 1024. wn with relu

--- example_configs/text2text/en-de/en-de-convs2s_dev.py
+++ example_configs/text2text/en-de/en-de-convs2s_dev.py
@@ -38,10 +38,10 @@ use_horovod = True
 
 max_steps = int((4500000 / (num_gpus * batch_size * iter_size)) * epoch_num)
 
-conv_act = gated_linear_units #gated_linear_units #tf.nn.relu tf.nn.tanh
+conv_act = None #gated_linear_units #tf.nn.relu tf.nn.tanh
 normalization_type = "weight_norm"  #weight_norm or "batch_norm" or None
 scaling_factor = math.sqrt(0.5) #changed here
-inti_var = None #1e-3
+inti_var = 1e-3
 
 base_params = {
   # iter_size can be used just with horovod
@@ -148,55 +148,6 @@ base_params = {
 
 }
 
-# train_params = {
-#   "data_layer": ParallelTextDataLayer,
-#   "data_layer_params": {
-#     "pad_vocab_to_eight": True,
-#     "src_vocab_file": data_root + "m_common.vocab",
-#     "tgt_vocab_file": data_root + "m_common.vocab",
-#     "source_file": data_root + "train.clean.en.shuffled.BPE_common.32K.tok",
-#     "target_file": data_root + "train.clean.de.shuffled.BPE_common.32K.tok",
-#     "delimiter": " ",
-#     "shuffle": shuffle_train,
-#     "shuffle_buffer_size": 25000,
-#     "repeat": True,
-#     "map_parallel_calls": 16,
-#     "prefetch_buffer_size": 2,
-#     "max_length": max_length,
-#   },
-# }
-#
-# eval_params = {
-#   "batch_size_per_gpu": 64,
-#   "data_layer": ParallelTextDataLayer,
-#   "data_layer_params": {
-#     "src_vocab_file": data_root+"m_common.vocab",
-#     "tgt_vocab_file": data_root+"m_common.vocab",
-#     "source_file": data_root+"wmt13-en-de.src.BPE_common.32K.tok",
-#     "target_file": data_root+"wmt13-en-de.ref.BPE_common.32K.tok",
-#     "delimiter": " ",
-#     "shuffle": False,
-#     "repeat": True,
-#     "max_length": max_length,
-#     "prefetch_buffer_size": 1,
-#     },
-# }
-#
-# infer_params = {
-#   "batch_size_per_gpu": 64,
-#   "data_layer": ParallelTextDataLayer,
-#   "data_layer_params": {
-#     "src_vocab_file": data_root+"m_common.vocab",
-#     "tgt_vocab_file": data_root+"m_common.vocab",
-#     "source_file": data_root+"wmt14-en-de.src.BPE_common.32K.tok",
-#     "target_file": data_root+"wmt14-en-de.src.BPE_common.32K.tok",
-#     "delimiter": " ",
-#     "shuffle": False,
-#     "repeat": False,
-#     "max_length": max_length,
-#     "prefetch_buffer_size": 1,
-#   },
-
 
 train_params = {
   "data_layer": ParallelTextDataLayer,
@@ -247,5 +198,56 @@ infer_params = {
     "repeat": False,
     "max_length": max_length,
   },
+
+
+# train_params = {
+#   "data_layer": ParallelTextDataLayer,
+#   "data_layer_params": {
+#     "pad_vocab_to_eight": True,
+#     "src_vocab_file": data_root + "m_common.vocab",
+#     "tgt_vocab_file": data_root + "m_common.vocab",
+#     "source_file": data_root + "train.clean.en.shuffled.BPE_common.32K.tok",
+#     "target_file": data_root + "train.clean.de.shuffled.BPE_common.32K.tok",
+#     "delimiter": " ",
+#     "shuffle": shuffle_train,
+#     "shuffle_buffer_size": 25000,
+#     "repeat": True,
+#     "map_parallel_calls": 16,
+#     "prefetch_buffer_size": 2,
+#     "max_length": max_length,
+#   },
+# }
+#
+# eval_params = {
+#   "batch_size_per_gpu": 64,
+#   "data_layer": ParallelTextDataLayer,
+#   "data_layer_params": {
+#     "src_vocab_file": data_root+"m_common.vocab",
+#     "tgt_vocab_file": data_root+"m_common.vocab",
+#     "source_file": data_root+"wmt13-en-de.src.BPE_common.32K.tok",
+#     "target_file": data_root+"wmt13-en-de.ref.BPE_common.32K.tok",
+#     "delimiter": " ",
+#     "shuffle": False,
+#     "repeat": True,
+#     "max_length": max_length,
+#     "prefetch_buffer_size": 1,
+#     },
+# }
+#
+# infer_params = {
+#   "batch_size_per_gpu": 64,
+#   "data_layer": ParallelTextDataLayer,
+#   "data_layer_params": {
+#     "src_vocab_file": data_root+"m_common.vocab",
+#     "tgt_vocab_file": data_root+"m_common.vocab",
+#     "source_file": data_root+"wmt14-en-de.src.BPE_common.32K.tok",
+#     "target_file": data_root+"wmt14-en-de.src.BPE_common.32K.tok",
+#     "delimiter": " ",
+#     "shuffle": False,
+#     "repeat": False,
+#     "max_length": max_length,
+#     "prefetch_buffer_size": 1,
+#   },
+
 
 }
