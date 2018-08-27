d786e8b9e7e82902de209c31353df5ac05ca7f89
Vitaly Lavrukhin <vlavrukhin@nvidia.com>
2018-08-27 16:37:21 +0000

    Update scripts, decoder

--- scripts/import_librivox.py
+++ scripts/import_librivox.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2018 Mozilla Corporation
+
+
 from __future__ import absolute_import, division, print_function
 
 # Make sure we can import stuff from util/
