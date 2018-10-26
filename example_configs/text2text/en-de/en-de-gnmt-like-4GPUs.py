a852aa7d94a43588763d3c68c94198866fe22a20
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2018-10-26 16:54:44 -0700

    try new gnmt config

--- example_configs/text2text/en-de/en-de-gnmt-like-4GPUs.py
+++ example_configs/text2text/en-de/en-de-gnmt-like-4GPUs.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 import tensorflow as tf
 
 from open_seq2seq.models import Text2Text
-from open_seq2seq.encoders import GNMTLikeEncoderWithEmbedding
+from open_seq2seq.encoders import GNMTLikeEncoderWithEmbedding_cuDNN
 from open_seq2seq.decoders import RNNDecoderWithAttention, \
   BeamSearchRNNDecoderWithAttention
 from open_seq2seq.data.text2text.text2text import ParallelTextDataLayer
@@ -16,10 +16,10 @@ data_root = "/data/wmt16-ende-sp/"
 base_model = Text2Text
 
 base_params = {
-  "use_horovod": False,
-  "num_gpus": 4,
-  "max_steps": 310000,
-  "batch_size_per_gpu": 32,
+  "use_horovod": True,
+  "num_gpus": 1, # use mpirun to specify 4 workers
+  "max_steps": 90000,
+  "batch_size_per_gpu": 64,
   "save_summaries_steps": 50,
   "print_loss_steps": 48,
   "print_samples_steps": 48,
@@ -38,28 +38,16 @@ base_params = {
     "use_staircase_decay": True,
     "min_lr": 0.0000005,
   },
-  #"summaries": ['learning_rate', 'variables', 'gradients', 'larc_summaries',
-  #              'variable_norm', 'gradient_norm', 'global_gradient_norm'],
+  "iter_size": 2,
   "max_grad_norm": 32768.0,
   "dtype": tf.float32, # to enable mixed precision, comment this line and uncomment two below lines
   #"dtype": "mixed",
   #"loss_scaling": "Backoff",
-  "encoder": GNMTLikeEncoderWithEmbedding,
+  "encoder": GNMTLikeEncoderWithEmbedding_cuDNN,
   "encoder_params": {
-    "initializer": tf.random_uniform_initializer,
-    "initializer_params": {
-      "minval": -0.1,
-      "maxval": 0.1,
-    },
-    "core_cell": tf.nn.rnn_cell.LSTMCell,
-    "core_cell_params": {
-        "num_units": 1024,
-        "forget_bias": 1.0,
-    },
-    "encoder_layers": 7,
-    "encoder_dp_input_keep_prob": 0.8,
-    "encoder_dp_output_keep_prob": 1.0,
-    "encoder_use_skip_connections": True,
+    "encoder_cell_type": "lstm",
+    "encoder_layers": 8,
+    "encoder_dp_output_keep_prob": 0.8,
     "src_emb_size": 1024,
   },
 
