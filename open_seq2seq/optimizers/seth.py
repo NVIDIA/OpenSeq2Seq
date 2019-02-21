2517f6881ff143a8d4eee23f3cfb66cd3f35ab70
borisgin <boris.ginsburg@gmail.com>
2019-02-21 15:39:53 -0800

    fixed bug in tf-seth config
    Signed-off-by: borisgin <boris.ginsburg@gmail.com>

--- open_seq2seq/optimizers/seth.py
+++ open_seq2seq/optimizers/seth.py
@@ -28,11 +28,82 @@ from tensorflow.python.training import training_ops
 from tensorflow.train import MomentumOptimizer
 import tensorflow as tf
 
-class SethOptimizer(MomentumOptimizer):
-  """Optimizer that implements the Layerwise ADAM
+class NoahOptimizer(MomentumOptimizer):
+  """Optimizer that implements SAG with ema-normalized gradients.
+   The algorithm is similar to Layerwise ADAM:
 
     ```
-    m_t <- beta * m_{t-1} + (1 - beta) * g_t
+     Layer-wise ema of grad norms per layer:
+       u_t <-- beta1 * u_{t-1} + (1 - beta1) * |g_t|
+
+    1st order SAG with grads normalized by u_t:
+       m_t <- beta1 * m_{t-1} + (1 - beta1) * ( g_t / u_t )
+
+    Weight update:
+       w_t <- w_{t-1} - lr_t * m_t
+    ```
+
+  """
+
+  def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.95,
+               epsilon=1e-6, ord =2, use_locking=False, name='NoahOptimizer'):
+    """Constructs a new NoahOptimizer
+
+    Args:
+      learning_rate: A `Tensor` or a floating point value.  The learning rate.
+      beta1: A `Tensor` or a float, used in ema for momentum.
+      beta2: A `Tensor` or a float, used in ema for grad norms,
+      epsilon: a float.  Default = 1e-6.
+      ord: a float. Ord if grad norms  Default = 2.
+      use_locking: If `True` use locks for update operations.
+      name: Optional, name prefix for the ops created when applying
+        gradients.  Defaults to "NoahOptimizer".
+      use_nesterov: If `True` use Nesterov Momentum.
+
+    """
+    super(NoahOptimizer, self).__init__(learning_rate, momentum=beta1,
+                 use_locking=use_locking, name=name, use_nesterov=False)
+    self._beta1 = beta1
+    self._beta2 = beta2
+    self._epsilon = epsilon
+    self._ord = ord
+    # Tensor versions, converted to tensors in apply_gradients
+    self._beta1_t = None
+    self._beta2_t = None
+    self._grads_ema = None
+
+  def apply_gradients(self, grads_and_vars, global_step=None, name=None):
+    self._beta1_t = ops.convert_to_tensor(self._beta1, name='beta1')
+    self._beta2_t = ops.convert_to_tensor(self._beta2, name='beta2')
+    len_vars=len(grads_and_vars)
+    # init ema variables if required
+    if self._grads_ema is None:
+      self._grads_ema = [None] * len_vars
+      for i in range(len_vars):
+        self._grads_ema[i] = tf.get_variable(name="grad_ema"+str(i),
+                                    shape=[], dtype=tf.float32,
+                                    initializer=tf.keras.initializers.Zeros(),
+                                    trainable=False)
+    # compute ema for each layer
+    for i, (grad, var) in enumerate(grads_and_vars):
+      g_norm=tf.norm(tensor=tf.cast(grad, tf.float32), ord=self._ord)
+      self._grads_ema[i] = tf.cond(tf.equal(self._grads_ema[i], 0.),
+             lambda: g_norm,
+             lambda: self._grads_ema[i]*self._beta2_t+g_norm*(1.-self._beta2_t)
+                                 )
+      g_factor = self._beta1_t / (self._grads_ema[i] + self._epsilon)
+      grad = tf.scalar_mul(g_factor, grad)
+      grads_and_vars[i] = (grad, var)
+
+    return super(NoahOptimizer, self).apply_gradients(
+      grads_and_vars, global_step=global_step, name=name)
+
+#------------------------------------------------------------------------------
+class SethOptimizer(MomentumOptimizer):
+  """Optimizer that implements the SAG with layerwise normalized grads
+
+    ```
+    m_t <- beta * m_{t-1} + (1 - beta) * (g_t/|g_t|)
     variable_t <- variable_{t-1} - lr_t * m_t
     ```
 
@@ -84,95 +155,3 @@ class SethOptimizer(MomentumOptimizer):
     grad = tf.scalar_mul((beta_t/(g_norm + self._epsilon)), grad)
     return super(SethOptimizer, self)._apply_sparse(grad, var)
 
-#---------------------------------------------------------------------
-
-class NoahOptimizer(MomentumOptimizer):
-  """Optimizer that implements algorithm "similar" to Layerwise ADAM
-
-    ```
-     Layer-wise ema of grad norms  - has length = num_of_layers
-       u_t <-- beta1 * u_{t-1} + (1 - beta1) * |g_t|
-
-    1st order momentum - has lenghts = num of vars:
-       m_t <- beta1 * m_{t-1} + (1 - beta1) * g_t / u_t
-
-    Weight update:
-       w_t <- w_{t-1} - lr_t * m_t /
-    ```
-
-  """
-
-  def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.95,
-               epsilon=1e-6, ord =2,
-               use_locking=False, name='NoahOptimizer'):
-    """Constructs a new NoahOptimizer
-
-    Args:
-      learning_rate: A `Tensor` or a floating point value.  The learning rate.
-      beta1: A `Tensor` or a floating point value, used in ema for 1st order momentum.
-      beta2: A `Tensor` or a floating point value, used in ema for layer grad norms,
-      epsilon: a floating point value.  Default = 1e-6.
-      ord: a floating point value. Ord if grad norms  Default = 2.
-      use_locking: If `True` use locks for update operations.
-      name: Optional name prefix for the operations created when applying
-        gradients.  Defaults to "SethOptimizer".
-      use_nesterov: If `True` use Nesterov Momentum.
-
-    """
-    super(NoahOptimizer, self).__init__(learning_rate, momentum=beta1,
-                                        use_locking=use_locking, name=name,
-                                        use_nesterov=False)
-    self._beta1 = beta1
-    self._beta2 = beta2
-    self._epsilon = epsilon
-    self._ord = ord
-    # Tensor versions, converted to tensors in apply_gradients
-    self._beta1_t = None
-    self._beta2_t = None
-    self._grads_ema = None
-
-  def apply_gradients(self, grads_and_vars, global_step=None, name=None):
-    self._beta1_t = ops.convert_to_tensor(self._beta1, name='beta1')
-    self._beta2_t = ops.convert_to_tensor(self._beta2, name='beta2')
-    len_vars=len(grads_and_vars)
-
-    if self._grads_ema is None:
-      self._grads_ema = [None] * len_vars
-      for i in range(len_vars):
-        self._grads_ema[i] = tf.get_variable(name="grad_ema"+str(i),
-                                    shape=[], dtype=tf.float32,
-                                    initializer=tf.keras.initializers.Zeros(),
-                                    trainable=False)
-
-    for i, (grad, var) in enumerate(grads_and_vars):
-      g_norm=tf.norm(tensor=tf.cast(grad, tf.float32), ord=self._ord)
-      self._grads_ema[i] = tf.cond(tf.equal(self._grads_ema[i], 0.),
-                  lambda: g_norm,
-                  lambda: self._grads_ema[i]*self._beta2_t + g_norm*(1.-self._beta2_t)
-                                 )
-      g_factor = self._beta1_t / (self._grads_ema[i] + self._epsilon)
-      grad = tf.scalar_mul(g_factor, grad)
-      grads_and_vars[i] = (grad, var)
-
-    return super(NoahOptimizer, self).apply_gradients(
-      grads_and_vars, global_step=global_step, name=name)
-
-
-  # def _prepare(self):
-  #   self._beta1_t = ops.convert_to_tensor(self._beta1, name='beta1')
-  #   self._beta2_t = ops.convert_to_tensor(self._beta2, name='beta2')
-  #   super(NoahOptimizer, self)._prepare()
-  #
-  # def _apply_dense(self, grad, var):
-  #   beta1_t = tf.cast(self._beta1_t, grad.dtype.base_dtype)
-  #   beta2_t = tf.cast(self._beta2_t, grad.dtype.base_dtype)
-  #   g_norm = tf.norm(tensor=tf.cast(grad, tf.float32), ord=2)
-  #
-  #   grad_s = tf.scalar_mul((beta_t / (g_norm + self._epsilon)), grad)
-  #   return super(NoahOptimizer, self)._apply_dense(grad_s, var)
-  #
-  # def _apply_sparse(self, grad, var):
-  #   beta_t = tf.cast(self._beta_t, grad.dtype.base_dtype)
-  #   g_norm = tf.norm(tensor=tf.cast(grad, tf.float32), ord=2)
-  #   grad_s = tf.scalar_mul((beta_t / (g_norm + self._epsilon)), grad)
-  #   return super(NoahOptimizer, self)._apply_sparse(grad_s, var)
