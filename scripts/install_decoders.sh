887f37128d8e769cc66fcf7c8f0881cfcf6be3f3
Vitaly Lavrukhin <vlavrukhin@nvidia.com>
2019-02-27 16:35:16 -0800

    Updated installation script for custom decoders
    Signed-off-by: Vitaly Lavrukhin <vlavrukhin@nvidia.com>

--- scripts/install_decoders.sh
+++ scripts/install_decoders.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -xe
 git clone https://github.com/PaddlePaddle/DeepSpeech
 mv DeepSpeech/decoders/swig_wrapper.py DeepSpeech/decoders/swig/ctc_decoders.py
 mv DeepSpeech/decoders/swig ./decoders
@@ -7,7 +8,7 @@ cd decoders
 sed -i "s/\.decode('utf-8')//g" ctc_decoders.py
 sed -i 's/\.decode("utf-8")//g' ctc_decoders.py
 sed -i "s/name='swig_decoders'/name='ctc_decoders'/g" setup.py
-sed -i "s/'swig_decoders'/'ctc_decoders', 'swig_decoders'/g" setup.py
+sed -i "s/py_modules=\['swig_decoders'\]/py_modules=\['ctc_decoders', 'swig_decoders'\]/g" setup.py
 chmod +x setup.sh
 ./setup.sh
 cd ..
