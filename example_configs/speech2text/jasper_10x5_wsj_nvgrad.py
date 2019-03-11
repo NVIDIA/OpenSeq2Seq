ccbf56d7829f7c90a116b359ab5b2ed3e16889f7
borisgin <boris.ginsburg@gmail.com>
2019-03-11 15:00:07 -0700

    removed augmentation from wsj config
    Signed-off-by: borisgin <boris.ginsburg@gmail.com>

--- example_configs/speech2text/jasper_10x5_wsj_nvgrad.py
+++ example_configs/speech2text/jasper_10x5_wsj_nvgrad.py
@@ -216,11 +216,11 @@ train_params = {
             "/data/WSJ/train_si284.csv"
             # Add synthetic csv here
         ],
-        "augmentation": {
-            'noise_level_min': -120,
-            'noise_level_max': -110,
-            'time_stretch_ratio': 0.1,
-        },
+        # "augmentation": {
+        #     'noise_level_min': -120,
+        #     'noise_level_max': -110,
+        #     'time_stretch_ratio': 0.1,
+        # },
         "syn_enable": False, # Change to True if using synthetic data
         "syn_subdirs": [], # Add subdirs of synthetic data
         "max_duration": 16.7,
