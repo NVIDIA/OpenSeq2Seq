76ddb9f8d6388591dd030581f99a54d24df2f7e4
borisgin <boris.ginsburg@gmail.com>
2019-03-13 16:58:39 -0700

    changed Copyright header
    Signed-off-by: borisgin <boris.ginsburg@gmail.com>

--- open_seq2seq/optimizers/novograd.py
+++ open_seq2seq/optimizers/novograd.py
@@ -1,4 +1,4 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+# Copyright (c) 2019 NVIDIA Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
