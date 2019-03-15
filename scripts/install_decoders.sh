ab8435f6b6c6e92083a3c5ac4f77b943301fb295
Vitaly Lavrukhin <vlavrukhin@nvidia.com>
2019-03-15 12:20:20 -0700

    Updated custom decoder to return beam score
    Signed-off-by: Vitaly Lavrukhin <vlavrukhin@nvidia.com>

--- scripts/install_decoders.sh
+++ scripts/install_decoders.sh
@@ -8,6 +8,7 @@ cd decoders
 sed -i "s/\.decode('utf-8')//g" ctc_decoders.py
 sed -i 's/\.decode("utf-8")//g' ctc_decoders.py
 sed -i "s/name='swig_decoders'/name='ctc_decoders'/g" setup.py
+sed -i "s/-space_prefixes\[i\]->approx_ctc/space_prefixes\[i\]->score/g" decoder_utils.cpp
 sed -i "s/py_modules=\['swig_decoders'\]/py_modules=\['ctc_decoders', 'swig_decoders'\]/g" setup.py
 chmod +x setup.sh
 ./setup.sh
