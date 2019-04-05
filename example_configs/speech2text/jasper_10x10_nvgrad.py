dcc0102e3cfe1b5e139cf783c5786185b3e003bf
borisgin <boris.ginsburg@gmail.com>
2019-04-04 18:29:42 -0700

    removed dropout in jasper 10x10
    Signed-off-by: borisgin <boris.ginsburg@gmail.com>

--- example_configs/speech2text/jasper_10x10_nvgrad.py
+++ example_configs/speech2text/jasper_10x10_nvgrad.py
@@ -93,93 +93,93 @@ base_params = {
                 "type": "conv1d", "repeat": 1,
                 "kernel_size": [11], "stride": [2],
                 "num_channels": 256, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.8,
+                "dilation":[1], # "dropout_keep_prob": 0.8,
             },
             {
                 "type": "conv1d", "repeat": layers_per_block,
                 "kernel_size": [11], "stride": [1],
                 "num_channels": 256, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.8,
+                "dilation":[1], # "dropout_keep_prob": 0.8,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": layers_per_block,
                 "kernel_size": [11], "stride": [1],
                 "num_channels": 256, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.8,
+                "dilation":[1], # "dropout_keep_prob": 0.8,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": layers_per_block,
                 "kernel_size": [13], "stride": [1],
                 "num_channels": 384, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.8,
+                "dilation":[1], # "dropout_keep_prob": 0.8,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": layers_per_block,
                 "kernel_size": [13], "stride": [1],
                 "num_channels": 384, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.8,
+                "dilation":[1], # "dropout_keep_prob": 0.8,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": layers_per_block,
                 "kernel_size": [17], "stride": [1],
                 "num_channels": 512, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.8,
+                "dilation":[1], # "dropout_keep_prob": 0.8,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": layers_per_block,
                 "kernel_size": [17], "stride": [1],
                 "num_channels": 512, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.8,
+                "dilation":[1], # "dropout_keep_prob": 0.8,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": layers_per_block,
                 "kernel_size": [21], "stride": [1],
                 "num_channels": 640, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.7,
+                "dilation":[1], # "dropout_keep_prob": 0.7,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": layers_per_block,
                 "kernel_size": [21], "stride": [1],
                 "num_channels": 640, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.7,
+                "dilation":[1], # "dropout_keep_prob": 0.7,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": layers_per_block,
                 "kernel_size": [25], "stride": [1],
                 "num_channels": 768, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.7,
+                "dilation":[1], # "dropout_keep_prob": 0.7,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": layers_per_block,
                 "kernel_size": [25], "stride": [1],
                 "num_channels": 768, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.7,
+                "dilation":[1], # "dropout_keep_prob": 0.7,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": 1,
                 "kernel_size": [29], "stride": [1],
                 "num_channels": 896, "padding": "SAME",
-                "dilation":[2], "dropout_keep_prob": 0.6,
+                "dilation":[2], # "dropout_keep_prob": 0.6,
             },
             {
                 "type": "conv1d", "repeat": 1,
                 "kernel_size": [1], "stride": [1],
                 "num_channels": 1024, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.6,
+                "dilation":[1], # "dropout_keep_prob": 0.6,
             }
         ],
 
-        "dropout_keep_prob": 0.7,
+        # "dropout_keep_prob": 0.7,
 
         "initializer": tf.contrib.layers.xavier_initializer,
         "initializer_params": {
