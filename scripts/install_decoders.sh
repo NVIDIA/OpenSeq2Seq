168289d9b57dcd2880546033c3cab9966499dfa6
Vitaly Lavrukhin <vlavrukhin@nvidia.com>
2019-04-08 23:36:20 -0700

    Added specific commit to installation script
    Signed-off-by: Vitaly Lavrukhin <vlavrukhin@nvidia.com>

--- scripts/install_decoders.sh
+++ scripts/install_decoders.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -xe
-git clone https://github.com/PaddlePaddle/DeepSpeech
+git clone https://github.com/PaddlePaddle/DeepSpeech 
+cd DeepSpeech
+git checkout a76fc69
+cd ..
 mv DeepSpeech/decoders/swig_wrapper.py DeepSpeech/decoders/swig/ctc_decoders.py
 mv DeepSpeech/decoders/swig ./decoders
 rm -rf DeepSpeech
