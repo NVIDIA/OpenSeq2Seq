048eef4e15192698a7fe16e370cfd7c9212417d6
Vitaly Lavrukhin <vlavrukhin@nvidia.com>
2019-04-10 13:10:49 -0700

    Updated Transformer-big checkpoint
    Signed-off-by: Vitaly Lavrukhin <vlavrukhin@nvidia.com>

--- example_configs/text2text/en-de/transformer-nvgrad.py
+++ example_configs/text2text/en-de/transformer-nvgrad.py
@@ -38,7 +38,7 @@ base_params = {
   "use_horovod": True,
   "num_gpus": 1, #8, # when using Horovod we set number of workers with params to mpirun
   "batch_size_per_gpu": 128,  # this size is in sentence pairs, reduce it if you get OOM
-  "max_steps":  400000,
+  "max_steps":  600000,
   "save_summaries_steps": 100,
   "print_loss_steps": 100,
   "print_samples_steps": 10000,
@@ -54,12 +54,12 @@ base_params = {
     "beta1": 0.95,
     "beta2": 0.99,
     "epsilon":  1e-08,
-    "weight_decay": 0.0001,
+    "weight_decay": 0.00001,
     "grad_averaging": False,
   },
   "lr_policy": poly_decay,
   "lr_policy_params": {
-    "learning_rate": 0.02,
+    "learning_rate": 0.04,
     "power": 2,
   },
 
