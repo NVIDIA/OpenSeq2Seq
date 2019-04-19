e60513f9366ff79c08fc88cff145104d0d6c6f7d
Yurii Rebryk <y.a.rebryk@gmail.com>
2019-04-19 11:49:15 -0700

    Add Kentar Pre-net
    Signed-off-by: Yurii Rebryk <y.a.rebryk@gmail.com>

---
+++ open_seq2seq/parts/kentavr/prenet.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+
+class Prenet:
+  def __init__(self,
+               n_layers,
+               hidden_size,
+               activation_fn,
+               dropout=0.5,
+               regularizer=None,
+               training=True,
+               dtype=None,
+               name="prenet"):
+    self.name = name
+    self.layers = []
+    self.dropout = dropout
+    self.training = training
+
+    for i in range(n_layers):
+      layer = tf.layers.Dense(
+        name="layer_%d" % i,
+        units=hidden_size,
+        use_bias=True,
+        activation=activation_fn,
+        kernel_regularizer=regularizer,
+        dtype=dtype
+      )
+      self.layers.append(layer)
+
+  def __call__(self, x):
+    with tf.variable_scope(self.name):
+      for layer in self.layers:
+        x = tf.layers.dropout(layer(x), rate=self.dropout, training=self.training)
+
+      return x
