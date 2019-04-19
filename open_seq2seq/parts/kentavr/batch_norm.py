b82ef345afa2dfa71354002cdbe60b329a80369c
Yurii Rebryk <y.a.rebryk@gmail.com>
2019-04-19 11:45:32 -0700

    Add BatchNorm1D
    Signed-off-by: Yurii Rebryk <y.a.rebryk@gmail.com>

---
+++ open_seq2seq/parts/kentavr/batch_norm.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+
+class BatchNorm1D:
+  def __init__(self, *args, **kwargs):
+    super(BatchNorm1D, self).__init__()
+    self.norm = tf.layers.BatchNormalization(*args, **kwargs)
+
+  def __call__(self, x, training):
+    with tf.variable_scope("batch_norm_1d"):
+      y = tf.expand_dims(x, axis=1)
+      y = self.norm(y, training=training)
+      y = tf.squeeze(y, axis=1)
+      return y
